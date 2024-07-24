5b751fb8be8a2164959de07533fa879ef0be3e96
hossein nazari <nazari.hossein28@gmail.com>
2024-07-24 19:18:17 +0330

    add Throttling API
    limiting api request

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 /*
 http://127.0.0.1:8000/api/login | POST method | in Headers add =>>> key:Accept and value:application/json
 
+example:
 {
     "email": "purdy.una@example.org",
     "password": "password"
@@ -27,8 +28,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class)->only(['index', 'show']);
-Route::apiResource('events', EventController::class)->except(['index', 'show'])->middleware('auth:sanctum');
+Route::apiResource('events', EventController::class)->except(['index', 'show'])->middleware('auth:sanctum')->middleware('throttle:api');
 
-// Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']); 
-Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update'])->only(['index', 'show', 'store']); 
-Route::apiResource('events.attendees', AttendeeController::class)->scoped()->only(['destroy'])->middleware('auth:sanctum'); 
+Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update'])->only(['index', 'show']) ; 
+Route::apiResource('events.attendees', AttendeeController::class)->scoped()->only(['destroy', 'store'])->middleware('auth:sanctum')->middleware('throttle:api'); 
